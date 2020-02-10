8c6349d01c6c6158d840cee4a4b79f050a9828bb
ddietterich <ddietterich@verily.com>
2020-02-10 13:32:46 -0500

    Initial copy and build of stairway

---
+++ src/main/java/bio/terra/stairway/exception/RetryException.java
@@ -0,0 +1,15 @@
+package bio.terra.stairway.exception;
+
+public class RetryException extends StairwayException {
+    public RetryException(String message) {
+        super(message);
+    }
+
+    public RetryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RetryException(Throwable cause) {
+        super(cause);
+    }
+}
