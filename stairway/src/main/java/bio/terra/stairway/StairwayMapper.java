97078fd87951fdbdd3c9d2c48a5a09c68cd18530
Rae Johanek <rjohanek@broadinstitute.org>
2025-11-05 10:52:36 -0500

    rm unused import

--- stairway/src/main/java/bio/terra/stairway/StairwayMapper.java
+++ stairway/src/main/java/bio/terra/stairway/StairwayMapper.java
@@ -2,7 +2,6 @@ package bio.terra.stairway;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
